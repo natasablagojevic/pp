c0e416a9cf5d18da4a0df27a432f7bab2b7450d8
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-03-15 19:22:06 +0100

    4 nedelja ex_1

---
+++ vezbe/04/1.py
@@ -0,0 +1,10 @@
+# prednosti constraint ogranicenja:  zakse je za programiranje, moje je da definisem problem, a alat ce da resi
+# mana constraint: nije efikasno jer se desava kombinatorna eksplozija i to kosta mnogo
+# map, filter, reduce:  funkcije viseg reda, mogu da prime argument kao funkciju ili da vrate funkciju
+    # map : na sve elemente primeni funkciju
+    # filter : izfiltriraj listu po nekom zadatom kriterijumu
+    # reduce : sazimam sve elemente u jednu vrednost
+
+# Komonentno prograimranje:
+#  - komonente medjusobno interaguju
+#  - glupo je da se sve komonente crtaju od nule
