de186d886883304484a877ac670d639846c94cf1
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-03-15 08:08:47 +0100

    Robot - staticke promenljive

--- vezbe/02/2.py
+++ vezbe/02/2.py
@@ -1,12 +1,27 @@
 class Robot:
     """Predstavlja robota sa imenom """
     
+    # klasan pristup
+    population = 0  
+    
     def __init__(self, name):
         self.name = name
+        Robot.population += 1
     
+    # deinicijalizacija    
+    def __del__(self):
+        Robot.population -= 1
+    
+        
     def say_hi(self):
         print(f'New message from {self.name}')
+    
+    @classmethod
+    def how_many(cls):
+        print(f'We have {cls.population} robots.')
         
+    # @staticmethod # za staticke metode u klasi
+    
 
 if __name__ == '__main__':
     r1 = Robot('R24')
@@ -16,4 +31,8 @@ if __name__ == '__main__':
     r1.say_hi()
     r2.say_hi()
     
+    r1.how_many()
     
+    del r1
+    
+    r2.how_many()
