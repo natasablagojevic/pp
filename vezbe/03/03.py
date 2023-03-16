81f66f0b95727318e1eb3f166b265475224e8f8f
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-03-16 16:13:34 +0100

    definisanje ogranicenja: svi jednaki, razliciti

---
+++ vezbe/03/03.py
@@ -0,0 +1,20 @@
+import constraint 
+
+problem = constraint.Problem()
+
+problem.addVariable('duzina', range(4))
+problem.addVariable('sirina', range(4))
+problem.addVariable('visina', range(10))
+
+# def ogranicenje(d, s, v):
+#     if d != s and d != v and s != v:
+#         return True
+    
+# problem.addConstraint(ogranicenje, ['duzina', 'sirina', 'visina'])
+# problem.addConstraint(constraint.AllDifferentConstraint())
+problem.addConstraint(constraint.AllEqualConstraint())
+
+solutions = problem.getSolutions()
+
+for solution in solutions:
+    print(solution)
