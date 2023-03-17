b3c22e2a538a0ea84af74109de41b51d64787d86
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-03-17 15:18:43 +0100

    digitron 2023

--- vezbe/04/digitron/main.py
+++ vezbe/04/digitron/main.py
@@ -2,36 +2,36 @@ import digitron
 from PyQt5 import QtCore, QtGui, QtWidgets
 import functools
 
-class MatfCalculator:
+class MatfCalculator(QtWidgets.QDialog, digirtron.UI_):
     def __init__(self):
-         super(MatfCalculator, self).__init__()
-         self.stupUI(self)
+        super(MatfCalculator, self).__init__()
+        self.stupUI(self)
 
-        lista_brojeva = [ 
-            (self.pushButton_br0, '0'),
-            (self.pushButton_br1, '1'), 
-            (self.pushButton_br2, '2'), 
-            (self.pushButton_br3, '3'), 
-            (self.pushButton_br4, '4'), 
-            (self.pushButton_br5, '5'), 
-            (self.pushButton_br6, '6'), 
-            (self.pushButton_br7, '7'), 
-            (self.pushButton_br8, '8'), 
-            (self.pushButton_br9, '9')
-        ]
+        lista_brojeva = [] 
+        lista_brojeva.append((self.pushButton_br0, '0'))
+        lista_brojeva.append((self.pushButton_br1, '1'))
+        lista_brojeva.append((self.pushButton_br2, '2')) 
+        lista_brojeva.append((self.pushButton_br3, '3')) 
+        lista_brojeva.append((self.pushButton_br4, '4')) 
+        lista_brojeva.append((self.pushButton_br5, '5')) 
+        lista_brojeva.append((self.pushButton_br6, '6')) 
+        lista_brojeva.append((self.pushButton_br7, '7')) 
+        lista_brojeva.append((self.pushButton_br8, '8')) 
+        lista_brojeva.append((self.pushButton_br9, '9'))
+    
     
         self.valid_ops = '+-*/%'
 
         for btn, br in lista_brojeva:
             button.clicked.connect(functools(self.__display_digit, br))
 
-        lista_operatora = [
-            (self.pushButton_plus, '+'),
-            (self.pushButton_minus, '-'),
-            (self.pushButton_puta, '*'),
-            (self.pushButton_mod, '%'),
-            (self.pushButton_podeljeno, '/')
-        ]
+        lista_operatora = []
+        lista_operatora.append((self.pushButton_plus, '+'))
+        lista_operatora.append((self.pushButton_minus, '-'))
+        lista_operatora.append((self.pushButton_puta, '*'))
+        lista_operatora.append((self.pushButton_mod, '%'))
+        lista_operatora.append((self.pushButton_podeljeno, '/'))
+    
 
         for btn, op in lista_operatora:
             button.clicked.connect(functools(self.__display_op, op))
