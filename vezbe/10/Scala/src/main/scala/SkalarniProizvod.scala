7462f2ab98c4dd258bb8d9434d215df830a5bb36
Natasa Blagojevic <78801410+natasablagojevic@users.noreply.github.com>
2023-06-05 15:28:32 +0200

    Update SkalarniProizvod.scala

--- vezbe/10/Scala/src/main/scala/SkalarniProizvod.scala
+++ vezbe/10/Scala/src/main/scala/SkalarniProizvod.scala
@@ -1,25 +1,25 @@
-//import org.apache.spark.{SparkConf, SparkContext}
-//
-//object SkalarniProizvod {
-//  def main(args: Array[String]): Unit = {
-//
-//    val path1: String = "jashf"
-//    val path2: String = "kjf"
-//
-//    /** CONFIGURATE */
-//    val config: SparkConf = new SparkConf()
-//      .setAppName("Skalarni Proizvod")
-//      .setMaster("local[4]")
-//    val ctx: SparkContext = new SparkContext()
-//
-//    /** MAIN */
-//    val v1 = ctx.textFile(path1).flatMap(l => l.split(", ")).map(e => e.toInt).repartition(3)
-//    val v2 = ctx.textFile(path1).flatMap(l => l.split(", ")).map(e => e.toInt).repartition(3)
-//
-//    val sp: Int = v1.zip(v2).map(p => p._1 * p._2).reduce((a, b) => a + b)
-//
-//    ctx.stop()
-//
-//    println(s"Skalarni proizvod je: $sp")
-//  }
-//}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SkalarniProizvod {
+ def main(args: Array[String]): Unit = {
+
+   val path1: String = "jashf"
+   val path2: String = "kjf"
+
+   /** CONFIGURATE */
+   val config: SparkConf = new SparkConf()
+     .setAppName("Skalarni Proizvod")
+     .setMaster("local[4]")
+   val ctx: SparkContext = new SparkContext()
+
+   /** MAIN */
+   val v1 = ctx.textFile(path1).flatMap(l => l.split(", ")).map(e => e.toInt).repartition(3)
+   val v2 = ctx.textFile(path1).flatMap(l => l.split(", ")).map(e => e.toInt).repartition(3)
+
+   val sp: Int = v1.zip(v2).map(p => p._1 * p._2).reduce((a, b) => a + b)
+
+   ctx.stop()
+
+   println(s"Skalarni proizvod je: $sp")
+ }
+}
